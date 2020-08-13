55d941601df71519f7bb964bee96073ea82a4fba
Ashish Dwivedi <piyush12e@gmail.com>
2020-08-13 08:50:11 +0530

    Updated Overview msg

--- src/components/overviewProfile/overview.js
+++ src/components/overviewProfile/overview.js
@@ -1,6 +1,6 @@
 import React from "react";
 
 const Overview = () => {
-  return <p> Ashish is Mainframe Developer with 5 years of experince.</p>;
+  return <p> Ashish is Mainframe Developer with 5 years of experince. Tools</p>;
 };
 export default Overview;
